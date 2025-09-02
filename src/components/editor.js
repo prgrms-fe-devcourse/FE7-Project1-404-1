e19dc493ae09bb0a7ffa5535148bfc65059ac1f4
stupilman <creworroad@naver.com>
2025-09-02 10:48:07 +0900

    feat: 디바운스 유틸 함수 추가 (cancel, flush 지원)

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,6 +1,8 @@
 export function initEditor({ mount }) {
   if (!mount) throw new Error("에디터에 마운트할 DOM 요소가 필요합니다.");
 
+  const state = { title: "", content: "" };
+
   // 에디터 컨테이너
   const editorContainer = document.createElement("div");
   editorContainer.className = "editor";
@@ -16,8 +18,18 @@ export function initEditor({ mount }) {
   contentTextarea.className = "editor__content";
   contentTextarea.placeholder = "내용을 입력하세요";
 
+  // 이벤트 연결
+  titleInput.addEventListener("input", () => {
+    state.title = titleInput.value;
+    // console.log("제목: ", state.title);
+  });
+  contentTextarea.addEventListener("input", () => {
+    state.content = contentTextarea.value;
+    // console.log("내용: ", state.content);
+  });
+
+  // DOM 연결 파트
   editorContainer.appendChild(titleInput);
   editorContainer.appendChild(contentTextarea);
-
   mount.appendChild(editorContainer);
 }
