db777a53eb083922dc84a5b977d25b6bdeedb426
stupilman <creworroad@naver.com>
2025-09-02 12:32:14 +0900

    fix: documents.js 파일 오타 수정

--- src/api/documents.js
+++ src/api/documents.js
@@ -2,6 +2,6 @@ import { http } from "./https.js";
 
 export const getRootDocuments = () => http.get("/documents");
 export const getDocument = (id) => http.get(`/documents/${id}`);
-export const createDocument = (data) => http.post(`documents`, data);
+export const createDocument = (data) => http.post(`/documents`, data);
 export const updateDocument = (id, data) => http.put(`/documents/${id}`, data);
-export const deleteDocument = (id) => http.delete(`documents/${id}`);
+export const deleteDocument = (id) => http.delete(`/documents/${id}`);
