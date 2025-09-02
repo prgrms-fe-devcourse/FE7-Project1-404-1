6997f996ade8480b342fa38bce583cb6e8a18339
stupilman <creworroad@naver.com>
2025-09-02 15:25:06 +0900

    feat: editor 컴포넌트 폴더 구조 분리 및 editor.css 추가

---
+++ src/main.js
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+=======
+import { initEditor } from "./components/editor/editor.js";
+
+const $editor = document.querySelector("#editor");
+initEditor({ mount: $editor, docId: 155414 }); // 임시 docId 사용
+>>>>>>> e9052ef (feat: editor 컴포넌트 폴더 구조 분리 및 editor.css 추가)
