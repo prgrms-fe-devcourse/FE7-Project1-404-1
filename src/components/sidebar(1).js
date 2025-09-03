4c0793db61eb906485286efef36cfdf7d90c83df
mandububsa <asm7179@gmail.com>
2025-09-03 09:47:04 +0900

    feat: import 수정

--- src/components/sidebar(1).js
+++ src/components/sidebar(1).js
@@ -1,5 +1,5 @@
-import { http } from "../api/https.js";
-
+import { getRootDocuments } from "../api/documents.js";
+import { createDocumentItem } from "./documentManager/createDocumentItems.js";
 // 각 문서 - 아이디(숫자)/제목(문자열)/자식목록(배열)
 function renderDocuments(docs) {
   let html = `<ul>`;
@@ -16,7 +16,7 @@ function renderDocuments(docs) {
 }
 
 export async function render() {
-  const docs = await http.get("/documents");
+  const docs = await getRootDocuments();
   const list = document.querySelector("body");
   list.innerHTML = renderDocuments(docs);
 
