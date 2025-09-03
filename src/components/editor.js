efbc44b7fff2e73b1ccdf8e60f9fe22efddb47d1
peachPetal <toneba3296@gmail.com>
2025-09-03 11:37:15 +0900

    Update editor.js documentAPI import하는 것으로 수정

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,5 +1,5 @@
 import { debounce } from "../utils/debounce.js";
-import { getDocument, updateDocument } from "../api/documentApi.js";
+import { getDocument, updateDocument } from "../api/documentAPI.js";
 
 export async function initEditor({ mount, docId }) {
   if (!mount) throw new Error("에디터에 마운트할 DOM 요소가 필요합니다.");
